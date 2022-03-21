58800f0227342c85472840921d590f29b74d6ae7
Your Name <bilol200414@gmail.com>
2022-03-22 02:47:53 +0500

    mosts blog one

---
+++ src/compponent/Most.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../steyls/most.css'
+import foto1 from '../img/IMAGE1.png'
+import foto2 from '../img/IMAGE (2).png'
+import foto3 from '../img/IMAGE (3).png'
+import foto4 from '../img/IMAGE (4).png'
+import foto5 from '../img/IMAGE (5).png'
+import foto6 from '../img/IMAGE (6).png'
+import smil1 from '../img/smil1.png'
+import smil2 from '../img/smil2.png'
+
+function Most() {
+  return (
+      <section>
+          <div className="container">
+              <div className="section_box">
+                  <div className="section_simile_box">
+                      <img src={foto1} alt="" />
+                      <h1>Still Standing Tall</h1>
+                      <p>Life begins at the end of your comfort zone.</p>
+                      <hr />
+                      <div className="simile">
+                          <div className="smil_photos">
+                              <img src={smil1} alt="" />
+                              <h6>William Wong</h6>
+                          </div>
+                          <div className="smil_title">
+                              <p>9/25/2015</p>
+                          </div>
+                      </div>
+                  </div>
+
