9b1e96055b0c9ef34955b90753081f3bff539d96
Your Name <bilol200414@gmail.com>
2022-03-22 02:45:31 +0500

    Create Content.jsx

---
+++ src/compponent/Content.jsx
@@ -0,0 +1,23 @@
+    <header>
+      <nav className="navbar">
+       <div className="container">
+       <a href="#" className="nav_link">Escape.</a>
+        <ul className='nav_list'>
+          <li className='nav_item'>
+            <a href="#">HOME</a>
+          </li>
+
+          <li className='nav_item'>
+            <a href="#">Categories</a>
+          </li>
+
+          <li className='nav_item'>
+            <a href="#">About</a>
+          </li>
+
+          <li className='nav_item'>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+       </div>
+      </nav>
